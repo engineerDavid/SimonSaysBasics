00eaa2e3d396659ead8a051986416e656cc1e389
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2020-07-01 19:11:26 -0500

    Bug Fix: The user can no longer hit multiply times to cheat the game, startover function was also fixed

--- game.js
+++ game.js
@@ -14,55 +14,50 @@ var buttonColours = ["green", "red", "yellow", "blue"];
 //used to keep track of the next random pattern the was generated
 var randomChosenColour = null;
 
+var start = true;
 
 //Start the game by listing if the user pressed ENTER
-$(document).on('keypress',function(e) {
-  if(e.which == 13) {
-      nextSequence();
-      if ($("body").is(".game-over") ) {
- 
-        $("body").removeClass("game-over");
-     
+$(document).on('keypress', function (e) {
+    if (start == true) {
+        if (e.which == 13) {
+            nextSequence();
+            if ($("body").is(".game-over")) {
+
+                $("body").removeClass("game-over");
+
+            }
+            start = false;
+
+        }
     }
-     
-  }
 });
-
-
 
 // This function generates the next sequence of the simon says pattern the user
 // must follow
-function userClicked(buttonClicked){
+function userClicked(buttonClicked) {
 
-  userChoseanColor = buttonClicked;
+    userChoseanColor = buttonClicked;
 
-  userClickedPattern.push(userChoseanColor);
+    userClickedPattern.push(userChoseanColor);
 
-  playAudio(userChoseanColor);
+    playAudio(userChoseanColor);
 
-  buttonAnnimation(userChoseanColor);
+    buttonAnnimation(userChoseanColor);
 
-  checkAnswer(userClickedPattern.length-1);
-  
-  
-  
+    checkAnswer(userClickedPattern.length - 1);
 
 };
 
-
-
 //Generates the next sequence of the game
 function nextSequence() {
-  userClickedPattern = [];
-
-   
+    userClickedPattern = [];
 
     var randomNumber = (Math.floor(Math.random() * 4));
 
     randomChosenColour = buttonColours[randomNumber];
 
     gamePattern.push(randomChosenColour);
-  
+
     playAudio(randomChosenColour);
 
     buttonAnnimation(randomChosenColour);
@@ -71,69 +66,57 @@ function nextSequence() {
 
     level++;
 
-
-
-
 }
 
+// Checks to see if the user has the correct answer as the random color
+// generated If it is wrong, the game ends
+function checkAnswer(curentLevel) {
 
-//Checks to see if the user has the correct answer as the random color generated
-//If it is wrong, the game ends
-function checkAnswer(curentLevel){
   
-    console.log(userClickedPattern[curentLevel]);
-    console.log(gamePattern[curentLevel]);
-  
-  if(gamePattern[curentLevel] === userClickedPattern[curentLevel]){
-    
-  
+    if (gamePattern[curentLevel] === userClickedPattern[curentLevel]) {
 
-  if(gamePattern.length === userClickedPattern.length){
-    
-    setTimeout(function () {
-     
-      nextSequence();
-    }, 1000);
+        if (gamePattern.length === userClickedPattern.length) {
 
-    
-  }
-}
+            setTimeout(function () {
 
-  else{
-    startOver();
+                nextSequence();
+            }, 1000);
 
-  };
+        }
+    } else {
+        startOver();
+
+    };
 
 }
-
-
-
-
-
 
 //plays the audio sound corisponding to the button chosen
-function playAudio(buttonClicked){
+function playAudio(buttonClicked) {
 
-  var audiofile = new Audio ('sounds/' + buttonClicked + '.mp3');
+    var audiofile = new Audio('sounds/' + buttonClicked + '.mp3');
 
-  audiofile.play(); 
+    audiofile.play();
 }
-
 
 //plays an animation corrisponding to the button chosean
-function buttonAnnimation(buttonClicked){
+function buttonAnnimation(buttonClicked) {
 
-  $("#"+buttonClicked).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + buttonClicked)
+        .fadeOut(100)
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
 
 }
 
+//Used to restart the game
+function startOver() {
+    playAudio("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over press ENTER to restart");
+    level = 0;
+    start = true;
+    gamePattern = [];
+    userClickedPattern = [];
 
-//Used to restart the game 
-function startOver(){
-  playAudio("wrong");
-  $("body").addClass("game-over");
-  $("#level-title").text("Game Over press ENTER to restart");
-  level = 0;
-
-  
 }
